e16a9d2a1b8a24b6faa2f02996286986993c8451
Benoit Havet <benhavet@WKMZTD031224.global.publicisgroupe.net>
2023-07-08 18:32:00 +0200

    add couple other tests

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
 const functions = {
-    add: (number1, number2) => number1 + number2
+    add: (number1, number2) => number1 + number2,
+    isNull: () => null, 
+    checkValue: x => x,
+    createUser: () => {
+        const user = { firstName: 'Toto'};
+        user['lastName'] = 'IsToto';
+        return user;
+    }
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -4,3 +4,30 @@ it('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
+
+it('Adds 2 + 3 to equal 5', () => {
+    expect(functions.add(2, 3)).toBe(5);
+});
+
+it('Adds 2 + 2 to not equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull()
+})
+
+test('Should be falsy', () => {
+    expect(functions.checkValue(null)).toBeFalsy()
+})
+
+test('LastName of user should be isToto', () => {
+    expect(functions.createUser()).toEqual({ firstName: 'Toto', lastName: 'IsToto' })
+})
+
+// Arrays
+test('Admin should be in usernames', () => {
+    usernames = ['john, karen', 'bob'];
+    expect(usernames).toContain('bob')
+})
